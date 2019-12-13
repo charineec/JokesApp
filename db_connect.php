04386892e73aa7cde92271770061115cdc87e813
charineec <43277476+charineec@users.noreply.github.com>
2019-12-13 14:40:48 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,10 +2,10 @@
 <?php
 
 //four variables to connect to the database
-$host = "localhost";
-$username = "root";
-$user_pass = "";
-$database_in_use = "test";
+$host = "hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "lnjjd7vwmaevgbme";
+$user_pass = "ey88qokxt9u0puh2";
+$database_in_use = "cqtmudibtr2hlsee";
 
 // create a database connection instance
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
